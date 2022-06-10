456b13aa87b38a5fb984f993994acf4f1ab993c3
NghiaVu76 <vunghiaxoan7@gmail.com>
2022-06-10 14:17:24 +0700

    xong ui

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Announcement from '../components/Announcement'
+import Categories from '../components/Categories'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
+import Products from '../components/Products'
+import Slide from '../components/Slide'
+
+const Home = () => {
+  return (
+    <div className="container">
+        <Announcement />
+        <Navbar />
+        <Slide/>
+        <Categories />
+        <Products />
+        <Newsletter />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
